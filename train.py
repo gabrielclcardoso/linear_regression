7feb54509b309d72c3b6768f0001d9c40ad68a62
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-07-12 19:17:48 -0300

    fix: correctly check step size limit

--- train.py
+++ train.py
@@ -3,8 +3,8 @@ import numpy as np
 import sys
 
 MIN_STEP = 1e-3  # What value should be put here?
-MAX_ITERATIONS = 10e5  # What value shoud be put here?
-LEARNING_RATE = 0.1  # What value shoud be put here?
+MAX_ITERATIONS = 1e4  # What value shoud be put here?
+LEARNING_RATE = 0.0000000001  # What value shoud be put here?
 
 
 def main():
@@ -21,7 +21,7 @@ def main():
     for iteration in range(int(MAX_ITERATIONS)):
         gradient = calculate_gradient(data, thetha)
         step_size = gradient * LEARNING_RATE
-        if np.any(step_size < MIN_STEP):
+        if np.all(abs(step_size) < MIN_STEP):
             break
         thetha -= step_size
     print(f'0 = {thetha[0]} | 1 = {thetha[1]}')  # Save to file afterwards
