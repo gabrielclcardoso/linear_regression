38cdcbb0b20215a27a453bc596dbd663c51c9f0f
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-07-12 16:43:10 -0300

    feat: implement gradient descent

--- train.py
+++ train.py
@@ -1,26 +1,42 @@
 import pandas as pd
+import numpy as np
 import sys
-from array import array
 
-# Read Database
-try:
-    data = pd.read_csv('data.csv')
-except Exception as e:
-    print(e, file=sys.stderr)
-    exit(1)
+MIN_STEP = 1e-3  # What value should be put here?
+MAX_ITERATIONS = 10e5  # What value shoud be put here?
+LEARNING_RATE = 0.1  # What value shoud be put here?
 
-# Gradient descent
-thetha = array('d', [0, 1])
-tmp_thetha = array('d', thetha)
-learning_rate = 0.1  # What value shoud be put here?
-max_iterations = 1e5  # What value shoud be put here?
-iterations = 0
-for i in data.itertuples():
-    tmp_thetha[0] = thetha[0]  # formula with thetha0
-    tmp_thetha[1] = thetha[1]  # formula with thetha1
-    # check step size
-    thetha[0] = tmp_thetha[0]
-    thetha[1] = tmp_thetha[1]
-    iterations += 1
-    if iterations == max_iterations:
-        break
+
+def main():
+    # Read Database
+    try:
+        data = pd.read_csv('data.csv')
+    except Exception as e:
+        print(e, file=sys.stderr)
+        exit(1)
+
+    # Perform gradien descent
+    thetha = np.array([0.0, 1])
+    step_size = np.array([0.0, 0])
+    for iteration in range(int(MAX_ITERATIONS)):
+        gradient = calculate_gradient(data, thetha)
+        step_size[0] = LEARNING_RATE * gradient[0]
+        step_size[1] = LEARNING_RATE * gradient[1]
+        if np.any(step_size < MIN_STEP):
+            break
+        thetha[0] -= step_size[0]
+        thetha[1] -= step_size[1]
+    print(f'0 = {thetha[0]} | 1 = {thetha[1]}')  # Save to file afterwards
+
+
+def calculate_gradient(data, thetha):
+    gradient = np.array([0.0, 0])
+    for car in data.itertuples():
+        estimated_price = thetha[0] + thetha[1] * car.km
+        gradient[0] += estimated_price - car.price
+        gradient[1] += (estimated_price - car.price) * car.km
+    return (gradient / len(data.index))
+
+
+if __name__ == '__main__':
+    main()
