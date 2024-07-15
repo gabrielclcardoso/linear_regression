ff29e9c4ab844785b78f4bc89e1c847b3ad85447
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-07-15 15:26:42 -0300

    refactor: extract reading thetha to function

--- predict.py
+++ predict.py
@@ -4,17 +4,22 @@ import sys
 
 def main():
     # Read thetha from training
+    thetha = read_thetha()
+    print(thetha)
+
+
+def read_thetha():
     try:
         file = open('thetha.bin', 'r')
         thetha = np.fromfile(file, dtype=np.float64)
         file.close()
-        if thetha.shape != (2,):
-            raise ValueError("File content is not an array of size 2 doubles")
-    except Exception as e:
-        print(e, file=sys.stderr)
+    except Exception:
+        thetha = np.array([0, 0], dtype=np.float64)
+    if thetha.shape != (2,):
+        print("File content is not an array of size 2 doubles",
+              file=sys.stderr)
         exit(1)
-
-    print(f'thetha values = {thetha}')
+    return thetha
 
 
 if __name__ == '__main__':
